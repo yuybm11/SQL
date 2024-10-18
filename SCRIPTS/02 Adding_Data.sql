b8d75acf6e601ee7bdcaacd2d5c156ca7e78ec36
Ángel MR <74621016+yuybm11@users.noreply.github.com>
2024-10-18 18:06:45 +0200

    filas de ejemplo

--- SCRIPTS/02 Adding_Data.sql
+++ SCRIPTS/02 Adding_Data.sql
@@ -24,34 +24,33 @@ VALUES
 INSERT INTO PUEBLOS (ID_PUEBLO, NOMBRE, ID_PROVINCIA)
 VALUES
 ('01', 'Málaga', '07'),
-('02', '', ''),
-('03', '', ''),
-('04', '', ''),
-('05', '', ''),
-('06', '', ''),
-('07', '', ''),
-('08', '', ''),
-('09', '', ''),
-('10', '', ''),
+('02', 'Frigiliana', '07'),
+('03', 'Setenil de las Bodegas', '08'),
+('04', 'Aracena', '01'),
+('05', 'Cazorla', '04'),
+('06', 'Pampaneira', '06'),
+('07', 'Mojácar', '05'),
+('08', 'Arcos de la Frontera', '08'),
+('09', 'Carmona', '02'),
+('10', 'Zuheros', '03'),
 
 INSERT INTO LIBROS
 VALUES
 ('01', 'Los Miserables', 'Victor Hugo', '10'),
-('02', '', '', '10'),
-('03', '', '', '10'),
-('04', '', '', '10'),
-('05', '', '', '10'),
-('06', '', '', '10'),
-('07', '', '', '10'),
-('08', '', '', '10');
+('02', 'Flamenco: Arqueología de lo Jondo', 'Antonio Manuel Rodríguez', '10'),
+('03', 'La Isla de la Mujer Dormida', 'Arturo Pérez-Reverte', '10'),
+('04', 'El flamenco a la luz de García Lorca', 'Agustín Gómez', '10'),
+('05', 'El Dominio Mental', 'Pedro Baños', '10'),
+('06', 'Territorio Negro', 'Manuel Marlasca', '10'),
+('07', 'El Boson de Higgs no te va a hacer la cama', 'Javier Santaolalla', '10');
 
 INSERT INTO SOCIOS
 VALUES
 ('01', 'Ángel', 'Martín Ramírez', '27/03/96', '05720634M', '660758959', 'amr0157@alu.medac.es', '01'),
-('02', 'Ángel', 'Martín Ramírez', '27/03/96', '05720634M', '660758959', 'amr0157@alu.medac.es', '01'),
-('03', 'Ángel', 'Martín Ramírez', '27/03/96', '05720634M', '660758959', 'amr0157@alu.medac.es', '01'),
-('04', 'Ángel', 'Martín Ramírez', '27/03/96', '05720634M', '660758959', 'amr0157@alu.medac.es', '01'),
-('05', 'Ángel', 'Martín Ramírez', '27/03/96', '05720634M', '660758959', 'amr0157@alu.medac.es', '01');
+('02', 'Alex', 'Cremento', '08/07/99', '68304578F', '698345098', 'alexcremento@gmail.com', '04'),
+('03', 'Dolores', 'Del Ano', '31/01/87', '04682930Y', '634278402', 'doloresdelano@gmail.com', '08'),
+('04', 'Elton', 'Tito', '25/12/66', '76529837S', '693560372', 'eltontito@gmail.com', '06'),
+('05', 'Ana', 'Busado de Hesa', '16/09/2003', '87629386F', '678354987', 'anabusadodehesa@gmail.com', '10');
 
 INSERT INTO PRESTAMOS (ID_PRESTAMO, PRESTADO, ID_BOOK, ID_SOCIO)
 VALUES
